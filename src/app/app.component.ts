5a6c807b06e6290e7b749f32578089e3c0aa7492
Dylan <dylan_mcneil@hotmail.co.uk>
2020-09-20 21:47:43 +0100

    Adding state to aid button disabling, pulling out event lengths into variables, fix bug where extra second was being added to times, added time that the physio will be finished by, added "mobile" npm command to serve the app so it's available on other devices on the network e.g. mobiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {State} from './enum/state';
 
 @Component({
   selector: 'app-root',
@@ -19,18 +20,30 @@ export class AppComponent {
   totalInterval;
   stretching = false;
   onGetReady = false;
+  finishedByTime: string;
+  private readonly STRETCH_TIME = 15;
+  private readonly REST_TIME = 5;
+  private readonly REPETITIONS = 15;
+  private readonly GET_READY_TIME = 3;
+  state: State;
+
+  constructor() {
+    this.state = State.NOT_STARTED;
+  }
 
   startPhysio(): void {
     this.getReady();
+    this.findFinishedByTime();
+    this.finishedByTime = this.findFinishedByTime();
   }
 
   getReady(): void{
-    console.log('hi');
+    this.state = State.GETTING_READY;
     this.onGetReady = true;
     this.displayText = 'Time until start';
-    this.timeLeft = 3;
+    this.timeLeft = this.GET_READY_TIME;
     this.interval = setInterval(() => {
-      if (this.timeLeft > 0){
+      if (this.timeLeft > 1){
         this.timeLeft--;
       } else {
         clearInterval(this.interval);
@@ -43,12 +56,13 @@ export class AppComponent {
   }
 
   oneExercise(): void {
+    this.state = State.STRETCHING;
     this.stretching = true;
     this.displayText = 'Current stretch duration left';
-    this.timeLeft = 1;
-    if (this.numberOfExercisesCompleted < 15){
+    this.timeLeft = this.STRETCH_TIME;
+    if (this.numberOfExercisesCompleted < this.REPETITIONS){
       this.interval = setInterval(() => {
-        if (this.timeLeft > 0){
+        if (this.timeLeft > 1){
           this.timeLeft--;
         } else{
           clearInterval(this.interval);
@@ -56,21 +70,28 @@ export class AppComponent {
         }
       }, 1000);
     } else{
+      this.state = State.COMPLETE;
       this.complete = true;
       clearInterval(this.totalInterval);
     }
   }
 
   pausePhysio(): void {
+    this.state = State.PAUSED;
     clearInterval(this.interval);
   }
 
+  isStartButtonDisabled(): boolean {
+    return !(this.state === State.NOT_STARTED || this.state === State.PAUSED || this.state === State.COMPLETE);
+  }
+
   private oneRest(): void {
+    this.state = State.RESTING;
     this.stretching = false;
     this.displayText = 'Rest duration left';
-    this.timeLeft = 1;
+    this.timeLeft = this.REST_TIME;
     this.interval = setInterval(() => {
-      if (this.timeLeft > 0){
+      if (this.timeLeft > 1){
         this.timeLeft--;
       } else{
         this.numberOfExercisesCompleted++;
@@ -93,5 +114,15 @@ export class AppComponent {
     const date = new Date(0);
     date.setSeconds(this.totalTimeElapsed);
     return date.toISOString().substr(11, 8);
+  }
+
+  private findFinishedByTime(): string {
+    const date = new Date();
+    date.setSeconds(date.getSeconds() + ((this.STRETCH_TIME + this.REST_TIME) * this.REPETITIONS) + this.GET_READY_TIME);
+    return date.toTimeString().substr(0, 8);
+  }
+
+  isPauseButtonDisabled(): boolean {
+    return this.state === State.PAUSED || this.state === State.NOT_STARTED;
   }
 }
