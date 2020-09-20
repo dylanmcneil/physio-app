afdf720dcc05fdb582c4804066ed9540a9f70981
Dylan <dylan_mcneil@hotmail.co.uk>
2020-09-20 13:42:18 +0100

    Adding get ready and word to display current action to do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // ng serve --host 192.168.0.17
   title = 'physio-app';
   started = false;
   timeLeft = 5;
@@ -14,15 +15,35 @@ export class AppComponent {
   displayText = '';
   complete = false;
   totalTimeElapsed = 0;
+  totalTimeElapsedString = '';
   totalInterval;
+  stretching = false;
+  onGetReady = false;
 
   startPhysio(): void {
-    this.started = true;
-    this.startTotalTimeElapsedTimer();
-    this.oneExercise();
+    this.getReady();
+  }
+
+  getReady(): void{
+    console.log('hi');
+    this.onGetReady = true;
+    this.displayText = 'Time until start';
+    this.timeLeft = 3;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0){
+        this.timeLeft--;
+      } else {
+        clearInterval(this.interval);
+        this.onGetReady = false;
+        this.started = true;
+        this.startTotalTimeElapsedTimer();
+        this.oneExercise();
+      }
+    }, 1000);
   }
 
   oneExercise(): void {
+    this.stretching = true;
     this.displayText = 'Current stretch duration left';
     this.timeLeft = 1;
     if (this.numberOfExercisesCompleted < 15){
@@ -45,6 +66,7 @@ export class AppComponent {
   }
 
   private oneRest(): void {
+    this.stretching = false;
     this.displayText = 'Rest duration left';
     this.timeLeft = 1;
     this.interval = setInterval(() => {
@@ -59,8 +81,17 @@ export class AppComponent {
   }
 
   private startTotalTimeElapsedTimer(): void {
+    this.totalTimeElapsedString = this.getDateString();
+
     this.totalInterval = setInterval(() => {
       this.totalTimeElapsed++;
+      this.totalTimeElapsedString = this.getDateString();
     }, 1000);
+  }
+
+  private getDateString(): string {
+    const date = new Date(0);
+    date.setSeconds(this.totalTimeElapsed);
+    return date.toISOString().substr(11, 8);
   }
 }
